b5aa4e8db7acc655709a4c1d0d33c7da21d7ccc8
Jonathan Baliellas <j.baliellas@hotmail.com>
2022-04-08 01:17:40 -0300

    Atualização do HTML, CSS e JS
    # HTML
    * Alteração do display da fórmula de text para label
    * Inclusão de evento no botão 'divisão'
    
    # JS
    * Criação da variável valorInput
    * Inclusão de novas operações aritméticas na função operacao()
    * Atualização da função limpaTudo()
    * Início da codificação da função igual(), porém carece de conclusão
    
    # CSS
    * Remoção do estilo txt_display
    * Inclusão do estilo lbl_display

--- Projetos/Calculadora/script.js
+++ Projetos/Calculadora/script.js
@@ -1,48 +1,100 @@
 let txt_edit = document.getElementById('txt_edit')
-let txt_display = document.getElementById('txt_display')
-let valor = 0
+let lbl_display = document.getElementById('lbl_display')
+let valorInput = 0
+let valor1 = 0
 let valor2 = 0
 let result = 0
 let operInic = true
 
 function click_num(num){
-    valor = valor * 10 + num
-    txt_edit.value = valor
+    valorInput = valorInput * 10 + num
+    txt_edit.value = valorInput
 }
 
 function operacao(operador){
-    switch (operador){
+    valor1 = valorInput
+    switch(operador){
         case 'x':
-            if (operInic == true){
-                result = valor                
+            if(operInic == true){
+                result = valor1
                 operInic = false
             }else{
-                result = result * valor
+                result = result * valor1
             }
             
-            valor = 0
+            valorInput = 0
             txt_edit.value = result
-            txt_display.value = result + ' x'
-            break        
+            lbl_display.innerHTML = result + ' x'
+            break
+
         case '/':
+            if(operInic == true){
+                result = valor1
+                operInic = false
+            }else{
+                result = result / valor1
+            }
+            
+            valorInput = 0
+            txt_edit.value = result
+            lbl_display.innerHTML = result + ' ÷'
             break
+
         case '+':
+            if(operInic == true){
+                result = valor1
+                operInic = false
+            }else{
+                result = result + valor1
+            }
+            
+            valorInput = 0
+            txt_edit.value = result
+            lbl_display.innerHTML = result + ' +'
             break
-        case '-':            
+
+        case '-':
+            if(operInic == true){
+                result = valor1
+                operInic = false
+            }else{
+                result = result - valor1
+            }
+            
+            valorInput = 0
+            txt_edit.value = result
+            lbl_display.innerHTML = result + ' -'    
     }
     
 }
 
 function igual(){
-
+    valor2 = valorInput
+    let comprimento = lbl_display.innerHTML.length
+    let operacao = lbl_display.innerHTML.substring(comprimento-1,comprimento)    
+    switch(operacao){
+        case 'x':
+            window.alert('multiplicação')
+            break
+        case '/':
+            window.alert('divisão')
+            break
+        case '+':
+            window.alert('adição')
+            break
+        case '-':
+            window.alert('subtração')
+            break
+    }
 }
 
 function limpaTudo(){
-    valor = 0
+    valorInput = 0
+    valor1 = 0
     valor2 = 0
     result = 0
     operInic = true
-    txt_display.value = ''
+    lbl_display.innerHTML = ''
     txt_edit.value = 0
 }
 
