5ecdd5380548be56044498712ace5cf7774b3d5b
Jonathan Baliellas <j.baliellas@hotmail.com>
2022-04-14 03:12:58 -0300

    Codificação de mais recursos e remoção de bugs no Projeto Calculadora
    #HTML
    - Inclusão de title em alguns botões: CE, C, BS, =
    - Substituição do <input text> por <p>
    
    #CSS
    - Alteração dos nomes das classes
    - Reestilização do novo lbl_display
    
    #JAVASCRIPT
    - Alteração das Id dos elementos;
    - Alteração na function click_num: limite de caracteres;
    - Recodificação da function operacao: agora se chama calcular; código otimizado, bugs removidos; agora o programa aguarda a inserção de novo valor em uma sequência de cálculos;
    - Exclusão da function igual;
    - Reajuste da function limpaTudo;
    - Criação das functions apagar, bkspc e pos_neg

--- Projetos/Calculadora/script.js
+++ Projetos/Calculadora/script.js
@@ -1,103 +1,92 @@
-let txt_edit = document.getElementById('txt_edit')
-let lbl_display = document.getElementById('lbl_display')
-let valorInput = 0
+let lbl_display = document.getElementById('lbl_display') // lbl do display
+let lbl_operacao = document.getElementById('lbl_operacao') //lbl da operação
+let valorInput = 0 //Valor do display
 let valor1 = 0
 let valor2 = 0
 let result = 0
-let operInic = true
+let operStandby = false
+let operadorMemo = ''
 
-function click_num(num){
-    valorInput = valorInput * 10 + num
-    txt_edit.value = valorInput
+function click_num(num){//Insere números no display
+    if (valorInput.toString().length < 11){ //Limita a quantidade de digitos a 11
+        valorInput = valorInput * 10 + num
+        lbl_display.innerHTML = valorInput
+    }    
 }
 
-function operacao(operador){
-    valor1 = valorInput
-    switch(operador){
-        case 'x':
-            if(operInic == true){
-                result = valor1
-                operInic = false
-            }else{
-                result = result * valor1
-            }
-            
-            valorInput = 0
-            txt_edit.value = result
-            lbl_display.innerHTML = result + ' x'
-            break
+function calcular(operador){//Realiza cálculos aritméticos conforme a operação escolhida
+    valor1 = valorInput //valor1 recebe o valor do display
+    valorInput = 0 //Zera o input para que o próximo número inicie do 0
 
-        case '/':
-            if(operInic == true){
-                result = valor1
-                operInic = false
-            }else{
-                result = result / valor1
-            }
-            
-            valorInput = 0
-            txt_edit.value = result
-            lbl_display.innerHTML = result + ' ÷'
-            break
-
-        case '+':
-            if(operInic == true){
-                result = valor1
-                operInic = false
-            }else{
-                result = result + valor1
-            }
-            
-            valorInput = 0
-            txt_edit.value = result
-            lbl_display.innerHTML = result + ' +'
-            break
-
-        case '-':
-            if(operInic == true){
-                result = valor1
-                operInic = false
-            }else{
-                result = result - valor1
-            }
-            
-            valorInput = 0
-            txt_edit.value = result
-            lbl_display.innerHTML = result + ' -'    
-    }
+    //Efetua o cálculo
+    if(operStandby == true){//Se houver dois valores para calcular, efetua a operação
+        valor2 = result //usado somente para o caso de operação final (=)
+        switch(operadorMemo){            
+            case 'x':
+                result = result * valor1     
+                break
     
-}
+            case '/':
+                result = result / valor1
+                break
+    
+            case '+':
+                result = result + valor1
+                break
+    
+            case '-':
+                result = result - valor1                
+        }
 
-function igual(){
-    valor2 = valorInput
-    let comprimento = lbl_display.innerHTML.length
-    let operacao = lbl_display.innerHTML.substring(comprimento-1,comprimento)    
-    switch(operacao){
-        case 'x':
-            window.alert('multiplicação')
-            break
-        case '/':
-            window.alert('divisão')
-            break
-        case '+':
-            window.alert('adição')
-            break
-        case '-':
-            window.alert('subtração')
-            break
+        //Finaliza a operação (=)
+        if(operador == '='){
+            operStandby = false
+        }
+    }else{//Se não houver dois valores para calcular, aguarda o segundo valor ser informado
+        result = parseFloat(lbl_display.innerHTML)//result recebe o primeiro valor
+        operStandby = true //Inicia o modo de cálculo
     }
+
+    //Mostra os valores no display
+    lbl_display.innerHTML = result
+    if(operador == '='){//No caso de operação final (=), exibe a equação completa
+        lbl_operacao.innerHTML = valor2 + ' ' + operadorMemo + ' ' + valor1 + ' ='
+    }else{
+        lbl_operacao.innerHTML = result + ' ' + operador
+    }
+
+    //Memoriza o operador
+    if(operador != '='){
+        operadorMemo = operador
+    }    
 }
 
-function limpaTudo(){
+function limpaTudo(){//Reinicia o programa (botão C)
     valorInput = 0
     valor1 = 0
     valor2 = 0
     result = 0
-    operInic = true
-    lbl_display.innerHTML = ''
-    txt_edit.value = 0
+    operStandby = false
+    lbl_operacao.innerHTML = ''
+    lbl_display.innerHTML = 0
 }
 
-function pos_neg(){//alterna entre positivo e negativo
+function apagar(){//Limpa o valor no display
+    valorInput = 0
+    lbl_display.innerHTML = valorInput
+}
+
+function bkspc(){//Exclui o último dígito
+    valorInput /= 10
+    valorInput = parseInt(valorInput) //Fica somente com a parte inteira
+    lbl_display.innerHTML = valorInput
+}
+
+function pos_neg(){//Alterna entre positivo e negativo
+    valorInput *= -1
+    lbl_display.innerHTML = valorInput
+}
+
+function decimal(){//Insere o ponto flutuante
 
 }
